ed2402fd1a581f04c5a1c3a63e38b7b2c3d154f5
ZainabNaveed13 <zainabnaveed137@gmail.com>
2024-12-20 23:13:49 +0500

    Added assembly code that takes hexa input and gives decimal output

---
+++ hexabin.asm
@@ -0,0 +1,34 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+	mov bx,43C2h
+	mov cx,16
+
+again:	rol bx,1
+	jc aa
+
+	mov dl,'0'
+	mov ah,02h
+	int 21h
+
+	jmp bb
+
+aa:	mov dl,'1'
+	mov ah,02h
+	int 21h
+
+bb:	dec cx
+	jnz again
+	
+	mov ah,4ch
+	int 21h
+
+main endp
+end main
+
+
+	
+
+	
